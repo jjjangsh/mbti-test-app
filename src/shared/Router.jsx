3a88af51b1b0354736702f0c1e69750ebf750971
jang sung hyun <hoko7022@gmail.com>
2024-09-10 01:21:07 +0900

    feat: Router 파일 추가

---
+++ src/shared/Router.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "../components/Layout";
+import ProtectedRoute from "../components/ProtectedRoute";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Profile from "../pages/Profile";
+import Signup from "../pages/Signup";
+import TestPage from "../pages/TestPage";
+import TestResultPage from "../pages/TestResultPage";
+
+const Router = () => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <BrowserRouter>
+      <Layout user={user} setUser={setUser}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login setUser={setUser} />} />
+          <Route path="/signup" element={<Signup />} />
+
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute user={user}>
+                <Profile user={user} setUser={setUser} />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/test"
+            element={
+              <ProtectedRoute user={user}>
+                <TestPage user={user} />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/results"
+            element={
+              <ProtectedRoute user={user}>
+                <TestResultPage user={user} />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
